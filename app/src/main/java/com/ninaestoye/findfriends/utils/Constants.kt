1dfca2ea655556cbc8f82b18af05813c09c8a93e
Nina <nina.manalo19@gmail.com>
2021-11-02 09:06:28 +0400

    hid constants

--- app/src/main/java/com/ninaestoye/findfriends/utils/Constants.kt
+++ app/src/main/java/com/ninaestoye/findfriends/utils/Constants.kt
@@ -3,6 +3,6 @@ package com.ninaestoye.findfriends.utils
 class Constants {
     companion object {
         const val BASE_URL = "https://dummyapi.io/"
-        const val APP_ID = "617f7a59df767fdcd744ea93"
+        
     }
 }
